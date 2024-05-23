a0f18771aed05dd079f4dbdf36ffc7ff1cfb2666
fannex20 <nikuradze.rati.19@gmail.com>
2024-05-24 00:59:43 +0400

    create proffesion and location container

--- invoice.js
+++ invoice.js
@@ -36,14 +36,34 @@ let invoiceId = "XM9141"
 // Status Info
 let statusInfo = document.getElementById("statusInfo");
 
+// Profession
+let profession = document.getElementById("profession");
+
+// Location Info
+let locationInfo = document.getElementById("locationInfo")
+
 async function fetchData() {
     const response = await fetch("data.json");
     const data = await response.json();
     const invoice = data.find(obj => obj.id == invoiceId);
 
     statusInfo.innerHTML += `
-    <div class="circle"></div>
-    <p>${invoice.status}</p>
+        <div class="circle"></div>
+        <p>${invoice.status}</p>
+    `
+
+    profession.innerHTML += `
+        <p class = "invoice-id"><span class = "symbol">#</span>${invoice.id} </p>
+        <p class = "description">${invoice.description} </p>   
+    `
+
+    locationInfo.innerHTML += `
+     <p class = "senderAddress">
+        ${invoice.senderAddress.street}
+         <br>${invoice.senderAddress.city}
+         <br>${invoice.senderAddress.postCode}
+         <br>${invoice.senderAddress.country}
+    </p>
     `
 }
 
